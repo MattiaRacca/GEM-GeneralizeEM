5609d76fa748bbc5afdfe1fb9ccb804dcc3c6005
MattiaRacca <mattia.rh@gmail.com>
2015-12-08 15:16:41 +0200

    Minor bug fix

--- Distributions/Exponential.m
+++ Distributions/Exponential.m
@@ -1,7 +1,7 @@
 function [ y ] = Exponential( mu, x )
 %EXPONENTIAL Exponential distribution
 %   http://se.mathworks.com/help/stats/exponential-distribution.html
-y = (1/mu)*exp(-x/mu);
+% y = (1/mu)*exp(-x/mu);
 y = pdf('Exponential',x,mu);
 end
 
--- Distributions/getNumberParameters.m
+++ Distributions/getNumberParameters.m
@@ -2,9 +2,9 @@ function [ K ] = getNumberParameters( distribution )
 %GETNUMBERPARAMETERS Gives the number of parameters for BIC computation
 K = 0;
 switch distribution.type
-        case 1
+        case 1              % Exponential
             K = K + 1;
-        case {2, 3}
+        case {2, 3}         % Guassian and Laplace
             K = K + 2;
 end
 end
