482308f0140e3b14f1cc90f92a8df1bb4f5853a4
MattiaRacca <mattia.rh@gmail.com>
2015-12-08 15:05:41 +0200

    Add Matlab version of pdf.

--- Distributions/Exponential.m
+++ Distributions/Exponential.m
@@ -2,5 +2,6 @@ function [ y ] = Exponential( mu, x )
 %EXPONENTIAL Exponential distribution
 %   http://se.mathworks.com/help/stats/exponential-distribution.html
 y = (1/mu)*exp(-x/mu);
+y = pdf('Exponential',x,mu);
 end
 
--- Distributions/Gaussian.m
+++ Distributions/Gaussian.m
@@ -1,6 +1,7 @@
 function [ y ] = Gaussian(mu, sigma, x)
 %GUASSIAN guassian probability distribution
 %	http://se.mathworks.com/help/stats/normal-distribution.html
-y = exp(-(x-mu).^2/(2*sigma.^2))/(sigma*sqrt(2*pi));
+% y = exp(-(x-mu).^2/(2*sigma.^2))/(sigma*sqrt(2*pi));
+y = pdf('Normal',x,mu,sigma);
 end
 
